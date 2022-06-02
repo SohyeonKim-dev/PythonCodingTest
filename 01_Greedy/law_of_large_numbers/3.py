3d38b7273aa012686ece9af8339a805345134c8c
SoHyeon Kim <happythgus89@gmail.com>
2022-06-02 15:23:07 +0900

    3

---
+++ 01_Greedy/law_of_large_numbers/3.py
@@ -0,0 +1,20 @@
+# 반복되는 수열 파악
+
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+count = int( m / (k+1)) * k
+count += m % ( k + 1)
+# first가 더해지는 횟수 -> count
+
+result = 0
+result += count * first 
+result += ( m - count) * second
+
+print(result)
